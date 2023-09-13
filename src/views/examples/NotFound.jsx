9ec413af9589758ef49ded33c5cfe57e2a05148d
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-13 09:53:00 -0300

    added: NotFound component

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+const NotFound = props =>{
+
+    return(
+        <div className="NotFound">
+            <h1>Não encontrado</h1>
+            <p>Não encontrado</p>
+        </div>
+    )
+
+}
+
+export default NotFound
