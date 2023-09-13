39e249c48715f630bd672a4e227f4f4ab184512d
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-13 09:52:31 -0300

    update: Routes added to Content.jsx

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,6 +3,9 @@ import './Content.css'
 import React from "react";
 
 import About from '../../views/examples/About';
+import Param from '../../views/examples/Param';
+import Home from '../../views/examples/Home';
+import NotFound from '../../views/examples/NotFound';
 
 const Content = props =>{
 
@@ -12,6 +15,8 @@ const Content = props =>{
             <Routes>
                 <Route path='/about' element={<About/>}/>
                 <Route path='/param:id' element={<Param/>}/> 
+                <Route path='/' exact element={<Home/>} />
+                <Route path='*' element={<NotFound/>}/>
             </Routes>
         </main>
     )
