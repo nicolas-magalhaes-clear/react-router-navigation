fd8893d385155d053168502adf81ba24a0fb698d
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-13 09:43:13 -0300

    added: Menu.jsx

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,19 @@
+import './Menu.css'
+import React from "react";
+
+const Menu = props => {
+
+    return (
+        <aside className="Menu">
+            <nav>
+                <ul>
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/">Sobre</a></li>
+                </ul>
+            </nav>
+        </aside>
+    )
+
+}
+
+export default Menu
