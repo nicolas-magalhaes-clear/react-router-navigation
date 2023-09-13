d39682e5a17c84d0bfe4379eab41d1d0e862a3d8
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-13 09:44:21 -0300

    added; Param.jsx

---
+++ src/views/examples/Param.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+const Param = props =>{
+
+    return(
+        <div className="Param">
+            <h1>Param</h1>
+            <p>Param</p>
+        </div>
+    )
+
+}
+
+export default Param
